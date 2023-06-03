b470b036ee2082e94ebf3940a63aa2b16f521a97
gazua300 <mesmo245@gmail.com>
2023-06-03 14:42:53 -0300

    atualizações no estilo da página de login

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,7 @@
 const nickname = document.getElementById('nickname')
-// const url = 'https://chat-jcnn.onrender.com'
-const url = 'http://localhost:3003'
+const btn = document.getElementById('submitForm')
+const url = 'https://chat-jcnn.onrender.com'
+// const url = 'http://localhost:3003'
 
 
 window.addEventListener('load', ()=>{
@@ -11,9 +12,42 @@ window.addEventListener('load', ()=>{
     }
 })
 
+
+nickname.addEventListener('input', ()=>{
+    btn.style.backgroundColor = 'green'
+
+    if(nickname.value !== ''){
+        btn.addEventListener('mouseover', handleMouseOver)
+        btn.addEventListener('mouseout', handleMouseOut)
+    }else{
+        btn.style.backgroundColor = 'rgb(50, 49, 49)'
+        btn.removeEventListener('mouseover', handleMouseOver)
+        btn.removeEventListener('mouseout', handleMouseOut)
+    }
+})
+
+const handleMouseOver = ()=>{
+    btn.style.backgroundColor = 'rgb(5, 66, 5)'
+    btn.style.fontWeight = 'bold'
+}
+
+const handleMouseOut = ()=>{
+    btn.style.backgroundColor = 'green'
+    btn.style.fontWeight = 'normal'
+}
+
+
 document.getElementById('form').addEventListener('submit', (e)=>{
     e.preventDefault()
+    signup()
+})
 
+document.getElementById('submitForm').addEventListener('click', ()=>{
+    signup()
+})
+
+
+const signup = ()=>{
     const body = {
         nickname: nickname.value
     }
@@ -23,10 +57,16 @@ document.getElementById('form').addEventListener('submit', (e)=>{
             'Content-type': 'application/json'
         },
         body: JSON.stringify(body)
-    }).then(res => res.text()).then(data=>{
+    }).then(res=>{
+        if(!res.ok && res.statusText === 'Unauthorized'){
+            throw new Error(`Digite um nome de usuário`)
+        }
+        
+        return res.text()
+    }).then(data=>{
         localStorage.setItem('user', data)
         location.href = './pages/chat/index.html'
     }).catch(e=>{
         alert(e.message)
     })
-})
+}
