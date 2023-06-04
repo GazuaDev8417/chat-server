150a9519d2a749334b790f1e4e287f617c0614a2
gazua300 <mesmo245@gmail.com>
2023-06-03 22:10:29 -0300

    finalizada implementação do botão para enviar mensagens sua estilização e funções

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const io = require('socket.io-client')
-const url = 'https://chat-jcnn.onrender.com'
-// const url = 'http://localhost:3003'
+// const url = 'https://chat-jcnn.onrender.com'
+const url = 'http://localhost:3003'
 const socket = io(url)
 const userList = document.querySelector('.user-list')
 const send = document.getElementById('send')
 const messages = document.querySelector('.messages')
 const currentUserIcon = document.getElementById('user')
-const popup = document.querySelector('.popup-prfile-photo')
 const inputFile = document.getElementById('inputFile')
 const imgProfile = document.getElementById('imgProfile')
 const input = document.getElementById('input')
+const btnSend = document.getElementById('btnSend')
 let user = localStorage.getItem('user')
 let users
 
@@ -79,6 +79,20 @@ socket.on('welcome', id=>{
 })
 
 
+input.addEventListener('input', ()=>{
+    const msgTemp = document.getElementById('msgTemp')
+    btnSend.style.display = 'block'
+    btnSend.removeAttribute('disabled')
+    msgTemp.style.display = 'none'
+
+    if(input.value === ''){
+        msgTemp.style.display = 'block'
+        btnSend.style.display = 'none'
+        btnSend.setAttribute('disbled', 'true')
+    }
+})
+
+
 send.addEventListener('submit', (event)=>{
     event.preventDefault()
     const messageData = {
@@ -87,6 +101,9 @@ send.addEventListener('submit', (event)=>{
     }
     socket.emit('message', messageData)
     input.value = ''
+    btnSend.setAttribute('disabled', 'true')
+    msgTemp.style.display = 'block'
+    btnSend.style.display = 'none'
     input.focus()
 })
 
